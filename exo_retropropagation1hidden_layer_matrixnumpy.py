3e9df500323a22baadcc1c64ef9be38e0045b93f
Damien MATTEI <damien.mattei@gmail.com>
2023-05-10 17:09:12 +0200

    tanh activation function

--- exo_retropropagation1hidden_layer_matrixnumpy.py
+++ exo_retropropagation1hidden_layer_matrixnumpy.py
@@ -3,16 +3,49 @@
 
 # modifications par D. Mattei
 
-from random import seed, uniform
-seed(1789)     # si vous voulez avoir les mêmes tirages aléatoires à chaque exécution du fichier !
-from math import exp, pow
+# python3.11 -O exo_retropropagation1hidden_layer_matrixnumpy.py
+
+
+from random import seed, uniform, randint
+#seed(1789)     # si vous voulez avoir les mêmes tirages aléatoires à chaque exécution du fichier !
+from math import exp, pow,pi , sin, tanh
 from MatrixNumPy import MatrixNumPy
 from time import time
+import sys
 
 
 # sigmoïde
 def sig(x):
+
+    try:
+        s = 1/(1+ exp(-x))
+    except OverflowError as e:
+        # Somehow no exception is caught here...
+        #print('OverflowError...')
+        #print("x=",x)
+        #sys.exit(1)
+        s = 0
+    except Exception as e:
+        print(e)
+    
+    return s
+
+def relu(x):
+    #print("x=",x)
+    r = max(0.0, x)
+    #print("r=",r)
+    return r
+
+def tanh_deriv(x):
+     return 1 - tanh(x)**2
+
+# sigmoïde
+def σ(x):
     return 1/(1+ exp(-x))
+
+def σࠤ(z):
+    return σ(z)*(1-σ(z))
+
 
 class ReseauRetroPropagation():
     
@@ -27,6 +60,7 @@ class ReseauRetroPropagation():
         
         # ne+1 in the matrix size because with add one column of bias in the matrix for each hidden neuron of the hidden layer "c"
         self.mat_ij = MatrixNumPy(lambda j,i: uniform(-1,1),nc,ne+1)  # self.mat_ij[j][i] == poids i->j
+        #self.mat_ij = MatrixNumPy(lambda j,i: 1.0,nc,ne+1) 
         
         self.z_j = nc * [0]     # valeurs z_j des neurones cachés
         self.grad_j = nc * [0]    # gradients locaux des neurones cachés
@@ -39,7 +73,7 @@ class ReseauRetroPropagation():
         
         self.nbiter = nbiter
         self.eta = eta                  # "learning rate" 
-        self.error = 0
+        self.error = 1
 
 
         
@@ -66,7 +100,10 @@ class ReseauRetroPropagation():
         z̃_j = mat_ij * z_i_1 # z̃_i = matrix * iterable (list here)
 
         # calcul des réponses des neurones cachés
-        z_j = list(map(sig,z̃_j)) 
+        # if you change this function you MUST make it match the derivative used in gradient computation and weight update!
+        #z_j = list(map(sig,z̃_j))
+        z_j = list(map(tanh,z̃_j))
+        #z_j = list(map(relu,z̃_j))
             
         # calcul des stimuli reçus par la couche de sortie
         mat_jk = self.mat_jk
@@ -76,8 +113,12 @@ class ReseauRetroPropagation():
         
         z̃_k = mat_jk * z_j_1 # matrix * iterable (list here)
 
-        # calcul des réponses des neurones cachés
-        z_k = list(map(sig,z̃_k))
+        # calcul des réponses de la couche de sortie
+
+        # if you change this function you MUST make it match the derivative in the gradient computation !
+        #z_k = list(map(sig,z̃_k))
+        z_k = list(map(tanh,z̃_k))
+        #z_k = list(map(relu,z̃_k))
         
         # update the variable when necessary
         self.z_j = z_j
@@ -94,87 +135,115 @@ class ReseauRetroPropagation():
 
         ip = 0                          # numéro de l'exemple courant
 
-        # TODO: take in account the error as stop point
-        for it in range(nbiter):   # le nombre d'itérations est fixé !
-            
-            error = 0.0                     # l'erreur totale pour cet exemple
-            
-            (entrees,sorties_attendues) = Lexemples[ip]         # un nouvel exemple à apprendre
-            
-            # PROPAGATION VERS L'AVANT
-            self.accepte_et_propage(entrees)       # sorties obtenues sur l'exemple courant, self.z_k et z_j sont mis à jour
-              
-            # RETRO_PROPAGATION VERS L'ARRIERE, EN DEUX TEMPS
 
-            # note: i just reference the variables for code readness (hide all the self keyword)
-            z_k = self.z_k # read-only variable
-            grad_k = self.grad_k
-
-            ns = len(z_k)
-            
-            # TEMPS 1. calcul des gradients locaux sur la couche k de sortie (les erreurs commises)
-            for k in range(ns):
-                grad_k[k] = sorties_attendues[k] - z_k[k]       # gradient sur un neurone de sortie (erreur locale)
-                error += pow(grad_k[k],2)                              # l'erreur quadratique totale
+        while self.error > 0.1: # 0.01: #  take in account the error as stop point
+            for it in range(nbiter):   # le nombre d'itérations est fixé !
                 
-            error *= 0.5
-            #print(it)
-            #print(error)
-            if it == nbiter-1 : self.error = error                     # mémorisation de l'erreur totale à la dernière itération
-
-            # modification des poids j->k
-            mat_jk = self.mat_jk # read/write data
-
-            z_i = self.z_i
-            z_j = self.z_j
-            nc = len(z_j)
-            eta = self.eta
+                error = 0.0                     # l'erreur totale pour cet exemple
             
-            # (test fait: modifier la matrice apres le calcul du gradient de la couche j , conclusion: ne change pas la convergence de l'algo)
-
-            self.modification_des_poids(mat_jk,eta,z_j,z_k,grad_k)
-
-            #print(mat_jk)
+                (entrees,sorties_attendues) = Lexemples[ip]         # un nouvel exemple à apprendre
             
-            # for k in range(ns): # line
-            #     for j in range(nc): # column , parcours les colonnes de la ligne sauf le bias
-            #         mat_jk[k][j+1] -= - eta * z_j[j] * z_k[k] * (1 - z_k[k]) * grad_k[k]
-            #     # and update the bias
-            #     mat_jk[k][0] -= - eta * 1.0 * z_k[k] * (1 - z_k[k]) * grad_k[k]
-                                
-            # Réponse à la question "b4" : T_{jk} = z_k * (1-z_k) * w_{jk}
+                # PROPAGATION VERS L'AVANT
+                self.accepte_et_propage(entrees)       # sorties obtenues sur l'exemple courant, self.z_k et z_j sont mis à jour
+            
+                # RETRO_PROPAGATION VERS L'ARRIERE, EN DEUX TEMPS
+
+                # note: i just reference the variables for code readness (hide all the self keyword)
+                z_k = self.z_k # read-only variable
+                grad_k = self.grad_k
+
+                ns = len(z_k)
 
 
-            
-            # TEMPS 2. calcul des gradients locaux sur la couche j cachée (rétro-propagation), sauf pour le bias constant
-            grad_j = self.grad_j
-            
-            for j in range(nc):
-                grad_j[j] = sum(z_k[k] * (1 - z_k[k]) * mat_jk[k,j+1] * grad_k[k] for k in range(ns))
                 
-            #print(grad_j)
-            
-            # modification des poids i->j
-            mat_ij = self.mat_ij
-             
-            self.modification_des_poids(mat_ij,eta,z_i,z_j,grad_j)
-            
-            # for j in range(nc):  # line
+                # TEMPS 1. calcul des gradients locaux sur la couche k de sortie (les erreurs commises)
                 
-            #     for i in range(ne): # column , parcours les colonnes de la ligne sauf le bias
-            #         mat_ij[j][i+1] -= -eta * z_i[i] * z_j[j] * (1 - z_j[j]) * grad_j[j]
+                for k in range(ns):
+                    grad_k[k] = sorties_attendues[k] - z_k[k]       # gradient sur un neurone de sortie (erreur locale)
+                    #print("grad_k[k] = ",grad_k[k])
+                    try:
+                        error += pow(grad_k[k],2) # l'erreur quadratique totale
+                    except OverflowError as e:
+                        error = sys.float_info.max
+                    except Exception as e:
+                        print(e)
+                error *= 0.5
+                #print(it)
+                #print(error)
+                if it == nbiter-1 :
+                    self.error = error                     # mémorisation de l'erreur totale à la dernière itération
+                    print("self.error=",self.error)
                     
-            #     # and update the bias
-            #     mat_ij[j][0] -= -eta * 1.0 * z_j[j] * (1 - z_j[j]) * grad_j[j]
+                # modification des poids j->k
+                mat_jk = self.mat_jk # read/write data
+
+                z_i = self.z_i
+                z_j = self.z_j
+                nc = len(z_j)
+                                        
+
+                eta = self.eta
+                #eta=0.01
+
+                # (test fait: modifier la matrice apres le calcul du gradient de la couche j , conclusion: ne change pas la convergence de l'algo)
+
+                self.modification_des_poids(mat_jk,eta,z_j,z_k,grad_k,tanh_deriv) #σࠤ) # Warning: Derivative MUST match the feedforward activation function of THE LAYER
                 
-                  
-            # et l'on passe à l'exemple suivant
+                #print(mat_jk)
             
-            ip = (ip + 1) % len(Lexemples)      # parcours des exemples en ordre circulaire
+                # for k in range(ns): # line
+                #     for j in range(nc): # column , parcours les colonnes de la ligne sauf le bias
+                #         mat_jk[k][j+1] -= - eta * z_j[j] * z_k[k] * (1 - z_k[k]) * grad_k[k]
+                #     # and update the bias
+                #     mat_jk[k][0] -= - eta * 1.0 * z_k[k] * (1 - z_k[k]) * grad_k[k]
+                                
+                # Réponse à la question "b4" : T_{jk} = z_k * (1-z_k) * w_{jk}
 
 
             
-    def modification_des_poids(self,M_i_o,eta,z_input,z_output,grad_i_o):
+                # TEMPS 2. calcul des gradients locaux sur la couche j cachée (rétro-propagation), sauf pour le bias constant
+                
+                grad_j = self.grad_j
+            
+                for j in range(nc):
+                    # must match the hidden activation function AND Derivative
+            
+                    #grad_j[j] = sum(z_k[k] * (1 - z_k[k]) * mat_jk[k,j+1] * grad_k[k] for k in range(ns)) # sigmoid
+                    grad_j[j] = sum((1 - tanh(z_k[k])**2) * mat_jk[k,j+1] * grad_k[k] for k in range(ns)) # tanh
+                    grad_j[j] = sum(tanh_deriv(z_k[k]) * mat_jk[k,j+1] * grad_k[k] for k in range(ns)) # tanh
+                    #grad_j[j] = sum(z_k[k] * mat_jk[k,j+1] * grad_k[k] if z_k[k] > 0  else 0 for k in range(ns)) # ReLU
+                
+                #print(grad_j)
+            
+                # modification des poids i->j
+                mat_ij = self.mat_ij
+             
+                self.modification_des_poids(mat_ij,eta,z_i,z_j,grad_j,tanh_deriv) #σࠤ) #tanh_deriv) # Warning: Derivative MUST match the feedforward activation function of THE LAYER AND of THE GRADIENT
+            
+                # for j in range(nc):  # line
+                
+                #     for i in range(ne): # column , parcours les colonnes de la ligne sauf le bias
+                #         mat_ij[j][i+1] -= -eta * z_i[i] * z_j[j] * (1 - z_j[j]) * grad_j[j]
+                
+                #     # and update the bias
+                #     mat_ij[j][0] -= -eta * 1.0 * z_j[j] * (1 - z_j[j]) * grad_j[j]
+                
+                
+
+                #eta = ((0.0001 - 1.0) / nbiter) * it + 1.0
+
+                #print(eta)
+            
+
+
+                # et l'on passe à l'exemple suivant
+            
+                #ip = (ip + 1) % len(Lexemples)      # parcours des exemples en ordre circulaire
+                ip = randint(0,len(Lexemples) - 1)
+
+
+    # modify hidden layers coefficients
+    def modification_des_poids(self,M_i_o,eta,z_input,z_output,grad_i_o,deriv):
         # the length of output and input layer with coeff. used for bias update             
         (len_layer_output, len_layer_input_plus1forBias) = M_i_o.dim()
         
@@ -184,12 +253,14 @@ class ReseauRetroPropagation():
         for j in range(len_layer_output):  # line
             
             for i in range(len_layer_input): # column , parcours les colonnes de la ligne sauf le bias
-                M_i_o[j,i+1] -= -eta * z_input[i] * z_output[j] * (1 - z_output[j]) * grad_i_o[j]
+                #M_i_o[j,i+1] -= -eta * z_input[i] * z_output[j] * (1 - z_output[j]) * grad_i_o[j] # sigmoid
+                #M_i_o[j,i+1] -= -eta * z_input[i] *  tanh_deriv(z_output[j]) * grad_i_o[j] # tanh
+                M_i_o[j,i+1] -= -eta * z_input[i] *  deriv(z_output[j]) * grad_i_o[j]
 
             # and update the bias
-            M_i_o[j,0] -= -eta * 1.0 * z_output[j] * (1 - z_output[j]) * grad_i_o[j]
-                
-
+            #M_i_o[j,0] -= -eta * 1.0 * z_output[j] * (1 - z_output[j]) * grad_i_o[j]
+            #M_i_o[j,0] -= -eta * 1.0 * tanh_deriv(z_output[j]) * grad_i_o[j]
+            M_i_o[j,0] -= -eta * 1.0 * deriv(z_output[j]) * grad_i_o[j]
             
                 
     def dump(self,n,msg):     # dump du réseau en entrant dans l'itération numéro n
@@ -215,7 +286,7 @@ if __name__ == '__main__':
     
    
     print('################## NOT ##################')
-    r1 = ReseauRetroPropagation(1,2,1,nbiter=10000,eta=0.5)
+    r1 = ReseauRetroPropagation(1,2,1,nbiter=10000,eta=0.1)
     Lexemples1 = [[[1],[0]],[[0],[1]]]
     START = time() ; r1.apprentissage(Lexemples1) ; END = time()
     r1.test(Lexemples1)
@@ -232,6 +303,18 @@ if __name__ == '__main__':
     #print("r2.mat_ij=",r2.mat_ij)
     #print("r2.mat_jk=",r2.mat_jk)
 
+
+
+    print('################## SINUS ##################')
+    r3 = ReseauRetroPropagation(1,100,1,nbiter=30000,eta=0.01)    # 2 entrées (+ bias), 3 couches de neurones cachés (+ bias), 1 neurone en sortie
+    Llearning = [ [[x],[sin(x)]] for x in [ uniform(-pi,pi) for n in range(1000)] ]
+    Ltest = [ [[x],[sin(x)]] for x in [ uniform(-pi/2,pi/2) for n in range(10)] ]
+    START = time() ; r3.apprentissage(Llearning) ; END = time()
+    print('APPRENTISSAGE sur {} itérations, time = {:.2f}s'.format(r3.nbiter,END-START))
+    r3.test(Ltest)
+    print("Error=") ; print(r3.error)
+
+    
     # COMPLEMENTS EN LIGNE
     from webbrowser import open as browse
     # Beaucoup de matériel sur la page Web de Geoffrey Hinton à Toronto, en particulier
